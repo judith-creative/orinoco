6be4c8447170f61918db003d35e3d51cbc7c613e
Judith <judith.schlich@hotmail.com>
2020-05-23 19:07:55 +0200

    change file name

--- DomModifier/singleProduct.js
+++ DomModifier/singleProduct.js
@@ -4,12 +4,12 @@ function displaySingleProduct() {
     itemCategory.innerHTML = `
         <h2 class="section-header">Ours en peluche</h2>
         <div class="shop-item">
-            <img class="shop-item-image" src="JWDP5/images/teddy_1.jpg" alt="teddy_1" />
+            <img class="shop-item-image" src="./JWDP5/images/teddy_1.jpg" alt="teddy_1" />
             <div class="shop-item-details">
-                <h3 class="shop-item-title">title</h3>
-                <h4 class="shop-item-price">price</h4>
+                <h3 class="shop-item-title">arnold</h3>
+                <h4 class="shop-item-price">33.00</h4>
                 <p class="shop-item-id">Article id : id</p>
-                <p class="shop-item-description">description</p>
+                <p class="shop-item-description">beschrijving</p>
                 <label for="customizeProduct"></label>
                 <select id="customizeProduct">
                     <option value="tan">brun clair</option>

ce44b74dda2ee55b48a837d176b1dbffe3e2b95c
Judith <judith.schlich@hotmail.com>
2020-05-23 19:07:21 +0200

    delete unused singleProductPage function

--- DomModifier/products.js
+++ DomModifier/products.js
@@ -30,11 +30,3 @@ function createSingleProduct (product) {
 function displayProducts(products) {
     products.forEach(createSingleProduct);
 }
-
-function singleProductPage(products) {
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        console.log(key);
-    }
-    //products.forEach(localStorage.key[i]);
-}
